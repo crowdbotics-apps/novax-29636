9aeb91a9e7f911210eaa95671d44983403e56306
Crowdbotics <team@crowdbotics.com>
2021-08-08 15:55:43 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "novax_29636Identifier",
+  url: "https://novax_29636Identifier.botics.co",
+  api: "https://novax_29636Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
